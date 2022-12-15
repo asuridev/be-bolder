2177fce5c20560942a16996f6a6ab32622fa99ad
antonio suarez <antoniosuarezrincon@gmail.com>
2022-12-15 08:45:00 -0500

    boton de pago

--- src/components/Pay.jsx
+++ src/components/Pay.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
 import {
   Box,
+  Button,
   CircularProgress,
   Paper,
   Table,
@@ -20,47 +21,58 @@ import { db } from "../firebase/config";
 export const Pay = () => {
   const [isLoading, setLoading] = useState(true);
   const [neto, setNeto] = useState(0);
+  const [infoBabies, setInfoBabies] = useState([]);
   const [dataWithDescuento, setDescuento] = useState([]);
   const navigate = useNavigate();
 
   const { data } = useSelector((state) => state.infoPassenger);
-  const { adults, kids } = useSelector((state) => state.passengers);
+  const { adults, kids, babies } = useSelector((state) => state.passengers);
   const { going, return: back } = useSelector((state) => state.bookings);
 
-  console.log(going);
-  console.log(back);
-
   const calcularDescuentos = async () => {
-    // const querySnapshot = await getDocs(collection(db, "fakeAPI"));
-    // const options = [];
-    // querySnapshot.forEach((doc) => {
-    //   options.push(doc.data());
-    // });
-    // setLoading(false);
-    // //setFlights([...options])
-    // console.log(options);
+    //determino la informacion de infantes
+    const arrBabies = [];
+    for (let i = 0; i < babies; i++) {
+      let descuento = 90;
+      let valor = going?.precio + (back?.precio ?? 0);
+      let total = valor - valor * (descuento / 100);
+      arrBabies.push({ key: `infante-${i + 1}`, descuento, valor, total });
+    }
+    //pendiente consultar base de datos para agregar descuentos
     const withDescuento = data.map((item) => {
       let descuento = 0;
       if (Number(item.age) > 65) {
         descuento += 3;
       }
 
+      //seguir trabajando aqui.
+
       // agregando informacion de total y descuento
       let valor = going?.precio + (back?.precio ?? 0);
       let total = valor - valor * (descuento / 100);
       return { ...item, valor, total, descuento };
     });
+    // fin
 
-    const totalPagar = withDescuento.reduce(
+    //calculo del valor total
+    const totalAdultosYniños = withDescuento.reduce(
       (acum, item) => (acum += item.total),
       0
     );
+    const totalPagar = arrBabies.reduce(
+      (acum, item) => (acum += item.total),
+      totalAdultosYniños
+    );
+
+    setInfoBabies(arrBabies);
     setNeto(totalPagar);
     setLoading(false);
     setDescuento(withDescuento);
-    console.log(withDescuento);
-    console.log(totalPagar);
   };
+
+  const onPay = ()=>{
+    
+  }
 
   useEffect(() => {
     calcularDescuentos();
@@ -74,7 +86,9 @@ export const Pay = () => {
         padding: "1rem 3rem",
       }}
     >
-      <Paper sx={{ backgroundColor: "#fff", padding: "2rem" }}>
+      <Paper
+        sx={{ backgroundColor: "#fff", padding: "2rem", position: "relative" }}
+      >
         <Box sx={{ display: "flex", marginBottom: "2rem" }}>
           <FlightTakeoffIcon sx={{ fontSize: "2rem" }} />
           <Typography
@@ -120,6 +134,19 @@ export const Pay = () => {
                       <TableCell align="right">{row.total}</TableCell>
                     </TableRow>
                   ))}
+                  {infoBabies.map((row) => (
+                    <TableRow
+                      key={row.key}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.key}
+                      </TableCell>
+                      <TableCell align="right">{row.valor}</TableCell>
+                      <TableCell align="right">{`${row.descuento}%`}</TableCell>
+                      <TableCell align="right">{row.total}</TableCell>
+                    </TableRow>
+                  ))}
                 </TableBody>
               </Table>
               <Typography
@@ -127,11 +154,26 @@ export const Pay = () => {
                 gutterBottom
                 variant="h5"
               >
-                {`Total a pagar: ${neto} COP`}
+                {`Total a pagar: $${neto} COP`}
               </Typography>
             </TableContainer>
           </Box>
         )}
+        <Button
+          sx={{
+            padding:'0.6rem 4rem',
+            position: "absolute",
+            bottom: "0",
+            left: "50%",
+            transform: "translate(-50%,50%)",
+          }}
+          onClick={onPay}
+          color="secondary"
+          size="large"
+          variant="contained"
+        >
+          Pagar
+        </Button>
       </Paper>
     </Box>
   );
